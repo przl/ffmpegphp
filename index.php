15b5722b000d42d3c6b89b020dfff0abd4e792b8
przl <nicdmc@gmail.com>
2016-02-25 16:52:33 +0000

    Added all files required
    added init files

---
+++ index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Mp4 Coverter</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">     
+
+<?php
+
+const VIDEO_DIR = 'completed/';
+const FAILED_DIR = 'incomplete/';
+const ONGOING_DIR = 'processing/input_processing/';
+const OUTGOING_DIR = 'processing/output_processing/';
+
+//get completed files
+$files = array_values(array_filter(scandir(VIDEO_DIR), function($file){
+    return preg_match('/^.*\.(3G2|3GP|ASF|AVI|FLV|M4V|MOV|MP4|MPG|RM|SRT|SWF|VOB|WMV|MKV)$/i', $file);
+}));
+
+//get ongoing conversions
+$ongoing_files = array_values(array_filter(scandir(ONGOING_DIR), function($file){
+    return preg_match('/^.*\.(3G2|3GP|ASF|AVI|FLV|M4V|MOV|MP4|MPG|RM|SRT|SWF|VOB|WMV|MKV)$/i', $file);
+}));
+
+//get failed conversions
+$failures = array_values(array_filter(scandir(FAILED_DIR), function($failure){
+    return preg_match('/^.*\.(3G2|3GP|ASF|AVI|FLV|M4V|MOV|MP4|MPG|RM|SRT|SWF|VOB|WMV|MKV)$/i', $failure);
+}));
+
+$names = $names = json_decode(file_get_contents('names.json'), true);
+
+?>
+
+
+<h1>Completed Conversions</h1>
+
+<table class="table">
+    <tr>
+        <th>Filename</th>
+        <th>Size</th>
+        <th>Time Created</th>
+    </tr>
+    <?php 
+
+    foreach($files as $file){
+
+        $original_name = $names['output'][$file];
+
+        echo " 
+                <tr>
+                    <td><a href='". VIDEO_DIR . "$file'</a>$original_name</td>
+                    <td> " . formatSizeUnits(filesize(VIDEO_DIR . $file)) . " </td>
+                    <td> " . date ("Y-m-d-H:i:s", filemtime(VIDEO_DIR . $file)) . "</td>
+                </tr>
+
+            ";
+    }
+?>
+
+</table>
+
+<h1>Ongoing Conversions</h1>
+
+<table class="table">
+    <tr>
+        <th>Filename</th>
+        <th>Size</th>
+        <th>Time Created</th>
+    </tr>
+    <?php 
+
+    foreach($ongoing_files as $ongoing){
+
+        $original_name = $names['input'][$ongoing];
+
+        echo " 
+                <tr>
+                    <td>$original_name</td>
+                    <td> " . formatSizeUnits(filesize(OUTGOING_DIR . $ongoing)) . "
+                    <td> " . date ("Y-m-d-H:i:s", filemtime(OUTGOING_DIR . $ongoing)) . "</td>
+                </tr>
+            ";
+    }
+?>
+
+</table>
+
+
+<h1>Failed Conversions</h1>
+
+<table class="table">
+    <tr>
+        <th>Original Name</th>
+        <th>File Name</th>
+        <th>Time Created</th>
+    </tr>
+    <?php 
+
+    foreach($failures as $failure){
+
+        $original_name = $names['input'][$failure];
+
+        echo " 
+                <tr>
+                    <td>$original_name</td>
+                    <td> $failure </td>
+                    <td> " . date ("Y-m-d-H:i:s", filemtime(FAILED_DIR . $failure)) . "</td>
+                </tr>
+
+            ";
+    }
+?>
+
+</table>
+
+</div>
+</body>
+</html>
+
+<?php 
+// Snippet from PHP Share: http://www.phpshare.org
+
+    function formatSizeUnits($bytes)
+    {
+        if ($bytes >= 1073741824)
+        {
+            $bytes = number_format($bytes / 1073741824, 2) . ' GB';
+        }
+        elseif ($bytes >= 1048576)
+        {
+            $bytes = number_format($bytes / 1048576, 2) . ' MB';
+        }
+        elseif ($bytes >= 1024)
+        {
+            $bytes = number_format($bytes / 1024, 2) . ' KB';
+        }
+        elseif ($bytes > 1)
+        {
+            $bytes = $bytes . ' bytes';
+        }
+        elseif ($bytes == 1)
+        {
+            $bytes = $bytes . ' byte';
+        }
+        else
+        {
+            $bytes = '0 bytes';
+        }
+
+        return $bytes;
+}
+?>
---
+++ readme.md
@@ -0,0 +1,41 @@
+# in home directory, make a directory to put the source code
+mkdir ~/ffmpeg_sources
+
+#get x264
+cd ~/ffmpeg_sources
+git clone --depth 1 git://git.videolan.org/x264
+cd x264
+PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static
+make
+make install
+make distclean
+
+#setup ffmpeg
+cd ~/ffmpeg_sources
+git clone http://source.ffmpeg.org/git/ffmpeg.git
+cd ffmpeg
+PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --extra-cflags="-I$HOME/ffmpeg_build/include" --extra-ldflags="-L$HOME/ffmpeg_build/lib" --bindir="$HOME/bin" --pkg-config-flags="--static" --enable-gpl --enable-libx264
+make
+make install
+make distclean
+hash -r
+
+#test is installed (should give a list of options)
+ffpmeg
+
+#create folders in the location of your page
+mkdir -m 777 completed && mkdir -m 777 incomplete && mkdir -m 777 logs && mkdir -m 777 processing && mkdir -m 777 queue && mkdir -m 777 processing/input_processing&& mkdir -m 777 processing/output_processing
+
+#instructions..
+put a file in the queue folder, run convert.php through apache (browser). If a file is successfully processed, the output will be found in the completed folder.
+If a conversion fails for any reason, the original file will be moved to incomplete folder.
+The index file will show completed conversions / ongoing conversions & incomplete (erroneous) conversions.
+
+#CRON
+for a cron task, put a hit to /convert.php every 5 minutes or so (depending on the size of the uploaded files and the )
+
+
+
+#ToDo
+- implement a queueing system per user (this process can be quite resource intensive and new ffmpeg processes can start killing off existing ones. At the moment this is handled, but it shouldn't really be a thing.
+- uploader tool for user interaction
